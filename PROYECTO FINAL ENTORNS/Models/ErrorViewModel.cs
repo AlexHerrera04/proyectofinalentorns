8f544a08202c09c621f646d59572353c90a8eea8
AlexHerrera04 <alex.herreb@gmail.com>
2025-05-14 10:41:55 +0200

    proyecto final 1 fase

---
+++ PROYECTO FINAL ENTORNS/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PROYECTO_FINAL_ENTORNS.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
